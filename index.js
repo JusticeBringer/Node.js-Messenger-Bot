032c7e959b412c73a26e7d921978bc0aee18cc2d
justicebringer <gabithebigg@gmail.com>
2020-08-31 17:29:21 +0300

    Modified files

--- index.js
+++ index.js
@@ -6,15 +6,6 @@ app.use(express.static(__dirname + '/views'));
 // Setting view engine as ejs
 app.set('view engine', 'ejs');
 
-// Static files
-app.use('/js', express.static('javascript'));
-app.use('/css', express.static('css'));
-
-// Front page
-app.get('/', function (req, res) {
-    res.render('html/index');
-});
-
 // Get messages
 app.get('/messages', async (req, res) => {
     res.send("Messages");
