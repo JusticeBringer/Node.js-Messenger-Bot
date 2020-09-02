61a83f830ffca97114b6da33250c48296d101bde
justicebringer <gabithebigg@gmail.com>
2020-09-02 18:27:13 +0300

    Using json

--- src/controllers/homepageController.js
+++ src/controllers/homepageController.js
@@ -1,12 +1,23 @@
+let fs = require('fs');
+
 let getHomepage = (req, res) => {
     return res.render("ejs/homepage.ejs");
 };
 
 let getMessages = (req, res) => {
     let all_messages = [];
-    for(let i = 0; i < localStorage.length; i ++){
-        all_messages.push(localStorage.getItem(toString(i)));
+
+    let rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function (){
+        if(rawFile.readyState === 4){
+            if(rawFile.status === 200 || rawFile.status == 0){
+                all_messages = rawFile.responseText;
+                return res.render("ejs/messages.ejs", {messages: all_messages});
+            }
+        }
     }
+    rawFile.send(null);
 
     return res.render("ejs/messages.ejs", {messages: all_messages});
 };
