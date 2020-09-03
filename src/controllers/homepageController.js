d7bed698c21ac854932a7741390b4b23f9ee2149
justicebringer <gabithebigg@gmail.com>
2020-09-03 17:36:51 +0300

    Working cloud messages - see messages

--- src/controllers/homepageController.js
+++ src/controllers/homepageController.js
@@ -4,13 +4,49 @@ let getHomepage = (req, res) => {
     return res.render("ejs/homepage.ejs");
 };
 
-let getMessages = (req, res) => {
-    try{
-        const all_messages = await Message.find();
-        res.render("ejs/messages.ejs", {messages: all_messages})
-    } catch (err){
-        res.render(err);
-    }
+let getMessages = async (req, res) => {
+    let MongoClient = require('mongodb').MongoClient;
+    let all_messages;
+
+    MongoClient.connect(
+        process.env.DB_CONNECTION, {
+            auth: {
+                user: process.env.MONGO_DB_USER,
+                password: process.env.MONGO_DB_PASSWORD
+            }
+        },
+        {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true}, 
+            function(err, client) {
+
+        if (err) throw err;
+        
+        console.log("Connected correctly to server for getting messages");
+
+        // Get database name
+        var db = client.db('MessengerBot')
+        
+        db.collection("messages").find({}).toArray(function(err, res) {
+            if (err) {
+                throw err;
+            }
+            
+            all_messages = res;
+            console.log("here")
+            console.log(res);
+            
+            client.close();
+        });
+    });
+
+
+    // waiting for messages
+    setTimeout(() => {
+        
+    console.log(JSON.stringify(all_messages));
+        res.render("ejs/messages.ejs", {messages: JSON.stringify(all_messages)});
+    }, 3000);
 };
 
 let getMessageId = (req, res) => {
