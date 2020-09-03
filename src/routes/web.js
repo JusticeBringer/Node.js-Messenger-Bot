7e1bf12b0ba812af471ae59b5c0e96fc592cb354
justicebringer <gabithebigg@gmail.com>
2020-09-03 20:59:26 +0300

    See individual messages

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,19 +1,24 @@
 import express from "express";
 import homepageController from "../controllers/homepageController";
 import chatBotController from "../controllers/chatBotController";
+import messagesController from "../controllers/messagesController";
+import headLinksController from "../controllers/headLinksController"
 
 let router = express.Router();
 
-let initWebRoutes = (app)=> {
+let initWebRoutes = async (app) => {
     router.get("/", homepageController.getHomepage);
-    router.get("/messages", homepageController.getMessages);
-    router.get("/headLinks", homepageController.getHeadLinks);
+    router.get("/messages", messagesController.getMessages);
+    router.get("/messages/:messId", messagesController.getMessageId);
+    router.delete("/messages/:messId", messagesController.deleteMessageById);
+
+    router.get("/headLinks", headLinksController.getHeadLinks);
     router.get("/webhook", chatBotController.getWebhook);
-    
+
     router.post("/", chatBotController.postMessage);
     router.post("/webhook", chatBotController.postWebhook);
 
-    return app.use("/", router);
+    return await app.use("/", router);
 };
 
 module.exports = initWebRoutes;
