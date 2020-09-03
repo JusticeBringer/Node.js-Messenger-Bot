7ee28ab0cd86e9a4ae903430e77d39aeeae01df8
justicebringer <gabithebigg@gmail.com>
2020-09-04 01:58:42 +0300

    Solved requirements

--- src/controllers/messagesController.js
+++ src/controllers/messagesController.js
@@ -1,38 +1,104 @@
-import { readFileSync } from 'fs';
+import { writeFileSync } from 'fs';
+var fs = require('fs');
+var path = require('path');
+var readStream = fs.createReadStream(path.join(__dirname, '/') + 'messages.json', 'utf8');
+
+function loadMessages(){
+	let data = '';
+	readStream.on('data', function(chunk) {
+		data += chunk;
+	}).on('end', function() {
+		MESS_S = JSON.parse(data);
+	});
+}
 
 // storing messages
-let MESS_S = {"id": 0, "text": "text"};
+let MESS_S = loadMessages();
 
 // caching messages for 1 minute
 setInterval(() => {
-	MESS_S = readFileSync("messages.json");
-	MESS_S = JSON.stringify(MESS_S);
-}, 2000);
+	MESS_S = loadMessages();
+}, 1000 * 60);
 
+// renders messages page
 let getMessages = (req, res) => {
-    return res.render("ejs/messages.ejs", {messages: JSON.stringify(MESS_S)});
+    res.render("ejs/messages.ejs", {messages: JSON.stringify(MESS_S)});
 };
+
+function lookMessage(mess_given_id){
+	for (let x of MESS_S){
+		// we will compare 2 id's
+		
+		// get their values
+		let first = "" + x.id;
+		let second = mess_given_id;
+
+		// compare char to char 
+		for(var i = 0; i < first.length; i++){
+			if(first[i] !== second[i])
+				break;
+		}
+
+		// if here -> we got the message
+		if (i === first.length){
+			return x.text;
+		}
+	}
+	return "";
+}
 
 // render a single message
 let getMessageId = (req, res) => {
-	let mess_id = req.params.messId;
-	console.log(mess_id);
-
-	for (let x in MESS_S){
-		console.log(x);
+	let val = lookMessage(req.params.messId);
+	if (val !== ""){
+		res.render("ejs/messages.ejs", {messages: val});
 	}
-	return res.render("ejs/messages.ejs", {messages: "MESS_S"});
+	// if here -> message not found
+	else{
+		res.render("ejs/messages.ejs", {messages: "Message not found"});
+	}
 };
 
 // delete a single message
 let deleteMessageById =  (req, res) => {
-	let msg = "";
-	
-	
-	res.render("ejs/messages.ejs", {messages: "Message deleted"});
+	let new_mess_s = [];
+	let mess_given_id = req.params.messId;
 
-	res.render("ejs/messages.ejs", {messages: "Message does not exist"});
+	let val = lookMessage(mess_given_id);
+	if (val !== ""){
+		for (let x of MESS_S){
+			// we will compare 2 id's
+			
+			// get their values
+			let first = "" + x.id;
+			let second = mess_given_id;
 	
+			// compare char to char 
+			for(var i = 0; i < first.length; i++){
+				if(first[i] !== second[i])
+					break;
+			}
+	
+			// we add every message different from the one we delete
+			if (i !== first.length){	
+				let new_mess = {};
+				new_mess.id = x.id;
+				new_mess.text = x.text;
+
+				new_mess_s.push(new_mess);
+			}
+		}
+
+		// update values
+		MESS_S = new_mess_s;
+		// write the new file
+		writeFileSync("/messages.json", MESS_S);
+
+		res.render("ejs/messages.ejs", {messages: "Message deleted"});
+	}
+	else{
+	res.render("ejs/messages.ejs", {messages: "Message does not exist"});
+	}
 };
 
 module.exports = {
