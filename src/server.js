5e10622ccd80d6fe9b6c226e63e64519ec44d02b
justicebringer <gabithebigg@gmail.com>
2022-04-07 16:10:00 +0300

    Add css to entire site

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
-const express      = require("express");
-const viewEngine   = require("./config/viewEngine");
+const express = require("express");
+const viewEngine = require("./config/viewEngine");
 const initWebRoute = require("./routes/web");
-const bodyParser   = require("body-parser");
-const cors         = require("cors");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 let app = express();
 
@@ -12,13 +12,15 @@ viewEngine(app);
 
 // use body-parser to post data
 app.use(bodyParser.json());
-app.use(cors({
-   'allowedHeaders': ['sessionId', 'Content-Type'],
-   'exposedHeaders': ['sessionId'],
-   'origin': '*',
-   'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-   'preflightContinue': false
- }));
+app.use(
+  cors({
+    allowedHeaders: ["sessionId", "Content-Type"],
+    exposedHeaders: ["sessionId"],
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+  })
+);
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // init all web routes
@@ -26,7 +28,7 @@ initWebRoute(app);
 
 let port = process.env.PORT || 8080;
 
-app.listen(port, ()=>{
-   console.log(`Application is running on port ${port}`) ;
-   console.log(`Press CTRL^C to stop the application`) ;
+app.listen(port, () => {
+  console.log(`Application is running on port ${port}`);
+  console.log(`Press CTRL^C to stop the application`);
 });
